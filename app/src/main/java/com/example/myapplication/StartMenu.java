333f3c8790abbf23b35bf4ccc52340ab51752d70
arbajmessiali <arbajmessiali@outlook.com>
2021-07-01 12:08:37 +0530

    change startMenu to fullScreen view

--- app/src/main/java/com/example/myapplication/StartMenu.java
+++ app/src/main/java/com/example/myapplication/StartMenu.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 
 public class StartMenu extends AppCompatActivity {
@@ -14,6 +15,7 @@ public class StartMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.start_menu);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         button = (Button) findViewById(R.id.startbtn);
         button.setOnClickListener(new View.OnClickListener() {
